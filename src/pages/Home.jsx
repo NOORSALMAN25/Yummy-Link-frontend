f345be0625fe3fda6539360a0bb02cf1e8578c4e
Malak1805 <mjma200018@gmail.com>
2025-08-05 12:02:39 +0300

    fatema and malak added the search functionality and connected them with the backend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,63 @@
 import Store from '../components/Store'
 import axios from 'axios'
 import Search from '../components/Search'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
-  const [SearchResult, setSearchResult] = useState([])
-  const [Searched, setSearched] = useState(false)
-  const [SearchQuery, setSearchQuery] = useState('')
+  const [searchResult, setSearchResult] = useState([])
+  const [searched, setSearched] = useState(false)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [stores, setStore] = useState([])
+
+  useEffect(() => {
+    const getStores = async () => {
+      const response = await axios.get(`http://localhost:3000/`)
+      console.log(response)
+      setStore(response.data)
+    }
+    getStores()
+  }, [])
+
+
+
+  const getSearchResults = async (e) => {
+    e.preventDefault()
+    setSearched(true)
+    const response = await axios.get(
+      `http://localhost:3000?search=${searchQuery}`
+    )
+    setSearchResult(response.data)
+    setSearchQuery('')
+  }
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value)
+  }
+
   return (
-    <>
-      <Search />
-      <Store />
-    </>
+    <div>
+      <Search
+        searchQuery={searchQuery}
+        handleChange={handleChange}
+        getSearchResult={getSearchResults}
+      />
+
+      <div className="search">
+        <h2>Search Results</h2>
+        <section>
+          {searchResult.map((store) => (
+            <Store key={store.id} />
+          ))}
+        </section>
+      </div>
+      <div>
+        <section>
+          {stores.map((store) => (
+            <Store key={store.id} store={store} />
+          ))}
+        </section>
+      </div>
+    </div>
   )
 }
 
